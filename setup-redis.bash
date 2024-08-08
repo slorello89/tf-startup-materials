65df17a7f8f86563f13c5f1501a147ab1b1eefe9
slorello89 <steve.lorello@redis.com>
2024-08-08 16:45:43 -0400

    first commit

---
+++ setup-redis.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update
+sudo apt-get install redis-stack-server -y
+cp ./redis-stack.conf /opt/redis-stack/etc
+
+sudo systemctl enable redis-stack-server
+sudo systemctl start redis-stack-server
